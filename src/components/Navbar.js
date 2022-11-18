eb2627bf8a7119d8bddaa55d365f41b810a8eaa5
luke-o-brien <lukeobrien02@gmail.com>
2022-11-18 23:09:58 +0000

    full updated navbar with topdown drop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 import styles from '../styles/Navbar.module.scss'
 // import { getLoggedInUserId } from "../lib/auth.js";
+import { getLoggedInUserId } from "../lib/auth";
 
 function Navbar() {
-
-  // const [hamburgerMenu , setHamburgerMenu] = React.useState(false)
-
-  // function handleClick() {
-  //   setHamburgerMenu(!hamburgerMenu)
-  // }
+  const [hamburgerMenu , setHamburgerMenu] = React.useState(false)
+  const loggedIn  = getLoggedInUserId()
+  console.log(loggedIn)
+  
+  
+  function handleClick() {
+    setHamburgerMenu(!hamburgerMenu)
+  }
 
   // function logout() {
   //   console.log("logout")
@@ -19,17 +22,60 @@ function Navbar() {
   //   localStorage.removeItem("_id");
   //   getLoggedInUserId()
   // }
+
+  // function loggedUser() {
+  //   const login = getLoggedInUserId()
+  //   setLoggedIn(login)
+  // }
   return (
-    <div>
-      <div className={styles.topnav}>
+    <div className={ hamburgerMenu ? `${styles.fullmenu}` : `${styles.topnav}`}>
+      <div className={ hamburgerMenu ? `${styles.fullmenutopdiv}` : `${styles.topnavtopdiv}`}>
         <div className={styles.leftitems}>
-          <div className={styles.logo}>EuroLink</div>
+          <div className={ hamburgerMenu ? `${styles.fullmenulogo}` : `${styles.logo}`}>EuroLink</div>
         </div>
         <div className={styles.rightitems}>
-          <Link to="/Login"><div className={styles.buttons}><p className={styles.buttontext}>Log In</p><i className="fa-regular fa-user"></i></div></Link>
-          <div className={styles.buttons}><p className={styles.buttontext}>Menu</p><i className="fa-solid fa-bars"></i></div>
+          { loggedIn ? <Link to="/dashboard" className={ hamburgerMenu ? `${styles.fullmenubuttons}` : `${styles.buttons}`} ><p className={styles.buttontext}>{`Dashboard`}</p><i className="fa-solid fa-user"></i></Link> : <Link to="/Login" className={ hamburgerMenu ? `${styles.fullmenubuttons}` : `${styles.buttons}`} ><p className={styles.buttontext}>Log In</p><i className="fa-regular fa-user"></i></Link>}
+          { hamburgerMenu ? <div className={styles.fullmenubuttons} onClick={handleClick}><p className={styles.buttontext}>Close</p> <i className="fa-solid fa-x"></i></div> : <div className={ hamburgerMenu ? `${styles.fullmenubuttons}` : `${styles.buttons}`} onClick={handleClick}><p className={styles.buttontext}>Menu</p><i className="fa-solid fa-bars"></i></div>}
         </div>
       </div>
+      { hamburgerMenu ? <div className={styles.fullmenubottom}>
+        <div className={styles.leftmenucontainer}>
+          <Link to="/SearchScreen"><div className={styles.menuItems}>
+            <span className={styles.menuIcon}><i className="fa-solid fa-ticket fa-fw"></i></span>
+            <h3 className={styles.bottomlink}>Plan a Journey</h3>
+          </div></Link>
+          <div className={styles.menuItems}>
+            <span className={styles.menuIcon}><i className="fa-solid fa-van-shuttle fa-fw"></i></span>
+            <h3 className={styles.bottomlink}> Onboard Experience</h3>
+          </div>
+          <div className={styles.menuItems}>
+            <span className={styles.menuIcon}><i className="fa-solid fa-book"></i> </span>
+            <h3 className={styles.bottomlink}>The EuroLink Story</h3>
+          </div>
+          <div className={styles.menuItems}>
+            <span className={styles.menuIcon}><i className="fa-solid fa-headset"></i> </span>
+            <h3 className={styles.bottomlink}>Contact Us</h3>
+          </div>
+        </div>
+        <div className={styles.hrcontainer}>
+          <div className={styles.hr}></div>
+        </div>
+        { loggedIn ? <div className={styles.leftmenucontainer}>
+          <div className={styles.welcomecontainer}>
+            <h2>Hi Luke</h2>
+            <h3>Welcome back!</h3>
+          </div>
+          <div className={styles.userlinks}>
+            <h3 className={styles.userlink}>Go To Your Dashboard </h3>    
+            <span className={styles.userIcon}><i className="fa-solid fa-arrow-right-long"></i></span>
+          </div>
+        </div> : <div className={styles.leftmenucontainer}>
+          <h2 className={styles.signuplink}>Planning your next adventure is easier with an account</h2>
+          <p className={styles.accountlink}>sign up</p>
+          <p className={styles.accountlink}>Log in</p>
+        </div>}
+        
+      </div> : null}
     </div>
   )
 }
